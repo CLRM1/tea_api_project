33e83187a468ccbe5d9bb1654125925836ac0dd6
Chris Romero <chrisleeromero1@gmail.com>
2022-07-25 16:11:50 -0600

    test: customer should exist and have attributes

---
+++ spec/models/customer_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe 'Customer' do
+  describe 'Validations' do
+    customer = Customer.create!(first_name: "Sophie", last_name: "Romero", email: "sophie@mail.com", address: "101 Main St. Denver, CO")
+    it { should validate_presence_of :first_name }
+    it { should validate_presence_of :last_name }
+    it { should validate_presence_of :email }
+    it { should validate_presence_of :address }
+  end 
+end
