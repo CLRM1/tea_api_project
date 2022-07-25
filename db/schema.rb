3edcd140129c5bf7eb47b1e1f8f35be79308be6d
Chris Romero <chrisleeromero1@gmail.com>
2022-07-25 16:44:55 -0600

    add subscription model migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_25_221458) do
+ActiveRecord::Schema.define(version: 2022_07_25_224347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 2022_07_25_221458) do
     t.string "last_name"
     t.string "email"
     t.string "address"
+  end
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.string "title"
+    t.float "price"
+    t.string "status"
+    t.string "frequency"
   end
 
 end
