f722ef0dcb099f660fe683c2ed83991d965f7557
Chris Romero <chrisleeromero1@gmail.com>
2022-07-26 10:23:37 -0600

    refactor: update associations test

--- spec/models/tea_spec.rb
+++ spec/models/tea_spec.rb
@@ -3,9 +3,6 @@ require 'rails_helper'
 RSpec.describe 'Model tests', type: :model do
   describe Tea do
     describe 'Validations' do
-      # customer = Customer.create!(first_name: "Sophie", last_name: "Romero", email: "sophie@mail.com", address: "101 Main St. Denver, CO")
-      # tea = Tea.create(title: 'Earl Grey', description: 'Berry and fruit flavors.', temperature: 145.50, brew_time: 6)
-      # subscription = customer.subscriptions.create!(title: 'ABC Tea Company', price: 20.50, status: 'active', frequency: 'monthly', tea_id: tea.id )
       it { should validate_presence_of :title }
       it { should validate_presence_of :description }
       it { should validate_presence_of :temperature }
@@ -13,7 +10,7 @@ RSpec.describe 'Model tests', type: :model do
     end 
 
     describe 'Associations' do
-      it { belong_to :customer }
+      it { should have_many :subscriptions }
     end
   end
 end
