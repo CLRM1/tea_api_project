62049cd5d5b73361cec49d4ace7d7e1e17dc5431
Chris Romero <chrisleeromero1@gmail.com>
2022-07-26 18:49:46 -0600

    cancel subscription request updates subscription

--- app/controllers/api/v1/subscription_controller.rb
+++ app/controllers/api/v1/subscription_controller.rb
@@ -1,13 +1,21 @@
 class Api::V1::SubscriptionController < ApplicationController
 
   def create
-    Subscription.create(title: params[:title], 
-                         price: params[:price], 
-                         status: params[:status], 
-                         frequency: params[:frequency], 
-                         customer_id: params[:customer_id], 
-                         tea_id: params[:tea_id])
+    Subscription.create(subscription_params)
     render json: {data: "Subscription added"},  status: 201
   end
 
+  def cancel
+    Subscription.where(title: params[:title]).first
+    subscription = Subscription.where(title: params[:title]).first
+    subscription.update(subscription_params)
+    subscription.save
+    render json: {data: "Subscription cancelled"},  status: 200
+  end
+
+  private
+
+  def subscription_params
+    params.permit(:title, :price, :status, :frequency, :customer_id, :tea_id)
+  end
 end
