70f852da2d8f5404ba5e8f43150b645e21fbfcc5
Chris Romero <chrisleeromero1@gmail.com>
2022-07-28 13:58:54 -0600

    condense show method for customer subscriptions

--- app/controllers/api/v1/customer_subscriptions_controller.rb
+++ app/controllers/api/v1/customer_subscriptions_controller.rb
@@ -1,8 +1,7 @@
 class Api::V1::CustomerSubscriptionsController < ApplicationController
   def show
    subscriptions = Subscription.where(customer_id: params[:id])
-   customer = Customer.where(id: params[:id])
-   if customer.count > 0
+   if Customer.where(id: params[:id]).count > 0
     render json: CustomerSubscriptionsSerializer.format_subscriptions(subscriptions), status: 200
    else
     render json: {data: "Error: customer not found"}, status: 404
